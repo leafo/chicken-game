41fbc76cdfa3704ba76f55e3dd9f083660333fca
leaf corcoran <leafot@gmail.com>
2016-04-03 00:00:38 -0700

    use cases, handle quit in regular event handler

--- main.scm
+++ main.scm
@@ -7,7 +7,7 @@
 
 (on-exit sdl2:quit!)
 
-(define quit-game #f)
+(define game-running #t)
 (define clear-color (sdl2:make-color 50 50 50))
 (define x 10)
 (define y 10)
@@ -26,12 +26,16 @@
     '(accelerated)))
 
 (define (handle-key-down ev)
-  (if (eqv? (sdl2:keyboard-event-sym ev) 'escape)
-    (set! quit-game #t)))
+  (case (sdl2:keyboard-event-sym ev)
+    ((escape)
+     (set! game-running #f))))
 
 (define (handle-event ev)
-  (if (eqv? (sdl2:event-type ev) 'key-down)
-    (handle-key-down ev)))
+  (case (sdl2:event-type ev)
+    ((quit)
+     (set! game-running #f))
+    ((key-down)
+      (handle-key-down ev))))
 
 (define (handle-events)
   (let ((done #f))
@@ -46,7 +50,7 @@
   (sdl2:render-fill-rect! renderer (sdl2:make-rect x y 10 10)))
 
 (define (main-loop)
-  (do-while (not (or quit-game (sdl2:quit-requested?)))
+  (do-while game-running
             (handle-events)
             (sdl2:render-draw-color-set! renderer clear-color)
             (sdl2:render-clear! renderer)
